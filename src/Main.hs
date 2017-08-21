6ca4e5218f6d797118ad5069f03c93cd5075ffc5
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2017-08-21 20:48:36 +0600

    Write minimal example of API usage

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Web.VKHS
+import qualified Data.Text.IO
+
+import Web.VKHS (runVK, defaultOptions, apiR)
+import Web.VKHS.API.Base (jsonEncodePretty)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  x <- runVK defaultOptions $ apiR "users.get" [("user_ids", "1,2"), ("fields", "last_seen")]
+  case x of
+    (Left e) ->  putStrLn $ show e
+    (Right a) -> Data.Text.IO.putStrLn $ jsonEncodePretty a
