d9823b7a0e2a9c9e6dac7e2969e1b113810a2391
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2017-08-22 15:21:31 +0600

    Create getMessagesR function

--- src/Main.hs
+++ src/Main.hs
@@ -3,13 +3,13 @@ module Main where
 
 import Control.Monad (mzero)
 import Data.Aeson (FromJSON(..), Value(..), (.:))
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import qualified Data.Text.IO
 import Data.UnixTime (fromEpochTime, UnixTime)
 import Foreign.C.Types (CTime(CTime))
 
-import Web.VKHS (runVK, defaultOptions, apiSimple)
-import Web.VKHS.API.Types (Sized)
+import Web.VKHS (runVK, defaultOptions, apiSimple, API, MonadAPI)
+import Web.VKHS.API.Types (Sized(..))
 
 type MessageId   = Int
 
@@ -33,9 +33,17 @@ instance FromJSON Message where
               <*> ((/= (0 :: Int)) <$> v .: "out")
   parseJSON _ = mzero
 
+getMessagesR :: (MonadAPI m x s) => Bool -> MessageId -> Int -> API m x (Sized [Message])
+getMessagesR out from count = apiSimple
+  "messages.get"
+  [ ("count", pack $ show count)
+  , ("offset", pack $ show from)
+  , ("out", if out then "1" else "0")
+  ]
+
 main :: IO ()
 main = do
-  x <- runVK defaultOptions $ apiSimple "messages.get" [("count", "2")]
+  x <- runVK defaultOptions $ getMessagesR True 0 4
   case x of
     (Left e) ->  putStrLn $ show e
     (Right a) -> putStrLn $ show (a :: Sized [Message])
