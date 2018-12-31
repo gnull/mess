211b3bda1405ea3464d191099ad8043719146862
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2018-12-31 17:35:34 +0300

    Add support for dowloading in multiple jobs
    This gave me like 12x speedup on my data with -j10.

--- app/cache/Main.hs
+++ app/cache/Main.hs
@@ -7,7 +7,8 @@ import Prelude hiding (readFile)
 
 import Data.Maybe (fromJust)
 import Data.List (genericLength)
-import Control.Monad (forM_)
+import Control.Monad (forM_, forM, replicateM)
+import Control.Arrow ((&&&))
 import System.FilePath ((</>), takeDirectory)
 import System.Directory (createDirectoryIfMissing)
 
@@ -21,13 +22,16 @@ import qualified System.Console.AsciiProgress as AP
 
 import Data.Default (Default(..))
 
-import Network.Wreq (get, responseBody)
+import Network.Wreq (responseBody)
+import Network.Wreq.Session (get, Session, newAPISession)
 import Control.Lens ((^.))
 import Network.URI (uriRegName, uriPort, uriAuthority, uriPath, parseAbsoluteURI)
 import Data.ByteString.Lazy (ByteString, writeFile)
 import Data.Binary (decode)
 
-import Control.Concurrent.MSem (new, with)
+import qualified Control.Concurrent.MSem as MSem (new, with)
+import Control.Concurrent.Async (async, wait)
+import Control.Concurrent.MVar (MVar, newMVar, modifyMVar, modifyMVar_)
 
 import Data.VkMess
   ( readFile
@@ -40,6 +44,7 @@ import Data.Semigroup((<>))
 data Options = Options
   { outDir :: FilePath
   , inFile :: FilePath
+  , jobs   :: Int
   }
 
 sample :: Parser Options
@@ -50,6 +55,10 @@ sample = do
   outDir <- argument str $
               metavar "DIR"
           <> help "Output directory for cached files"
+  jobs <- option auto $
+             long "jobs"
+          <> short 'j'
+          <> help "The number of parallel download jobs"
   pure Options {..}
 
 optparser :: IO Options
@@ -59,9 +68,9 @@ optparser = execParser opts
       ( fullDesc
      <> progDesc "Cache all media files refered by a mess database obtained with mess-fetch")
 
-getHTTP :: FilePath -> IO ByteString
-getHTTP url = do
-  r <- get url
+getHTTP :: Session -> FilePath -> IO ByteString
+getHTTP ses url = do
+  r <- get ses url
   pure $ r ^. responseBody
 
 getURIPath :: FilePath -> FilePath
@@ -70,14 +79,27 @@ getURIPath p = (uriRegName a ++ uriPort a) </> (dropWhile (== '/') $ uriPath u)
     u = fromJust $ parseAbsoluteURI p
     a = fromJust $ uriAuthority u
 
-processUrl :: AP.ProgressBar -> FilePath -> FilePath -> IO ()
-processUrl pb wd u = do
-  b <- getHTTP u
+processUrl :: AP.ProgressBar -> FilePath -> FilePath -> Session -> IO ()
+processUrl pb wd u ses = do
+  b <- getHTTP ses u
   let f = wd </> getURIPath u
   let d = takeDirectory f
   createDirectoryIfMissing True d
   Data.ByteString.Lazy.writeFile f b
   AP.tick pb
+
+pushMVar :: a -> MVar [a] -> IO ()
+pushMVar x = flip modifyMVar_ $ pure . (x:)
+
+popMVar :: MVar [a] -> IO a
+popMVar = flip modifyMVar $ pure . (tail &&& head)
+
+withMVar :: MVar [a] -> (a -> IO b) -> IO b
+withMVar m a = do
+  v <- popMVar m
+  res <- a v
+  pushMVar v m
+  pure res
 
 main :: IO ()
 main = do
@@ -85,6 +107,9 @@ main = do
   urls <- getSnapshotUrls <$> decode <$> readFile inFile
   let index = unlines $ flip map urls $ \u -> u ++ " " ++ getURIPath u
   Prelude.writeFile (outDir </> "index.txt") index
+  sem <- MSem.new jobs
+  ses <- newMVar =<< replicateM jobs newAPISession
   AP.displayConsoleRegions $ do
     pb <- AP.newProgressBar $ def { AP.pgTotal = genericLength urls }
-    forM_ urls $ processUrl pb outDir
+    as <- forM urls $ async . MSem.with sem . withMVar ses . processUrl pb outDir
+    forM_ as wait
