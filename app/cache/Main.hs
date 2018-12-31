6f7400016da7499e2637283da7a3c908d035ed22
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2018-12-31 16:04:11 +0300

    Add mess-cache for caching vk content

--- app/cache/Main.hs
+++ app/cache/Main.hs
@@ -1,10 +1,73 @@
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ApplicativeDo              #-}
+
 module Main where
+
+import Prelude hiding (readFile)
+
+import Data.Maybe (fromJust)
+import Control.Monad (forM_)
+import System.FilePath ((</>), takeDirectory)
+import System.Directory (createDirectoryIfMissing)
 
 import Network.Wreq (get, responseBody)
 import Control.Lens ((^.))
-import Data.ByteString.Lazy.Char8 (unpack)
+import Network.URI (uriRegName, uriPort, uriAuthority, uriPath, parseAbsoluteURI)
+import Data.ByteString.Lazy (ByteString, writeFile)
+import Data.Binary (decode)
+
+import Control.Concurrent.MSem (new, with)
+
+import Data.VkMess
+  ( readFile
+  , getSnapshotUrls
+  )
+
+import Options.Applicative
+import Data.Semigroup((<>))
+
+data Options = Options
+  { outDir :: FilePath
+  , inFile :: FilePath
+  }
+
+sample :: Parser Options
+sample = do
+  inFile <- argument str $
+              metavar "DUMP"
+          <> help "Input file with mess database"
+  outDir <- argument str $
+              metavar "DIR"
+          <> help "Output directory for cached files"
+  pure Options {..}
+
+optparser :: IO Options
+optparser = execParser opts
+  where
+    opts = info (sample <**> helper)
+      ( fullDesc
+     <> progDesc "Cache all media files refered by a mess database obtained with mess-fetch")
+
+getHTTP :: FilePath -> IO ByteString
+getHTTP url = do
+  r <- get url
+  pure $ r ^. responseBody
+
+getURIPath :: FilePath -> FilePath
+getURIPath p = (uriRegName a ++ uriPort a) </> (dropWhile (== '/') $ uriPath u)
+  where
+    u = fromJust $ parseAbsoluteURI p
+    a = fromJust $ uriAuthority u
 
 main :: IO ()
 main = do
-  r <- get "http://httpbin.org/get"
-  putStrLn $ unpack $ r ^. responseBody
+  Options {..} <- optparser
+  urls <- getSnapshotUrls <$> decode <$> readFile inFile
+  let index = unlines $ flip map urls $ \u -> u ++ " " ++ getURIPath u
+  Prelude.writeFile (outDir </> "index.txt") index
+  forM_ urls $ \u -> do
+    b <- getHTTP u
+    let f = outDir </> getURIPath u
+    let d = takeDirectory f
+    createDirectoryIfMissing True d
+    Data.ByteString.Lazy.writeFile f b
