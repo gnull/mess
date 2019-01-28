52c63b2016f3e4b7519c8f3026f54f82b9b23507
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2019-01-29 01:42:52 +0300

    Add an option to mess-fetch for passing access token

--- app/fetch/Main.hs
+++ app/fetch/Main.hs
@@ -8,7 +8,6 @@ import Prelude hiding (writeFile)
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Binary (encode)
-import Data.Maybe (fromMaybe)
 
 import Options.Applicative
 import Data.Semigroup((<>))
@@ -34,33 +33,31 @@ import App.Fetch (fetch)
 
 data Options = Options
   { outFile  :: FilePath
-  , email    :: Maybe String
-  , pass     :: Maybe String
+  , creds    :: Either (String, String) String -- (email, pass) or token
   , verb     :: Verbosity
   }
 
 sample :: Parser Options
 sample = do
-  outFile <- argument str $
-              metavar "FILE"
-          <> help "Output file"
-  verb <- (flag' Debug $
-               long "debug"
-            <> short 'd'
-            <> help "Enable debug output")
-          <|> (flag Normal Trace $
-               long "verbose"
-            <> short 'v'
-            <> help "Enable verbose (trace) output")
-  email <- optional $ strOption $
-             long "login"
-          <> short 'l'
-          <> help "User login"
-  pass <- optional $ strOption $
-             long "pass"
-          <> short 'p'
-          <> help "User password"
-  pure Options {..}
+    outFile <- argument str $
+                metavar "FILE"
+            <> help "Output file"
+    verb <- (flag' Debug $
+                 long "debug"
+              <> short 'd'
+              <> help "Enable debug output")
+            <|> (flag Normal Trace $
+                 long "verbose"
+              <> short 'v'
+              <> help "Enable verbose (trace) output")
+    creds <- (Left <$> emailPass) <|> (Right <$> token)
+    pure Options {..}
+  where
+    emailPass = do
+      email <- strOption $ long "login" <> short 'l' <> help "User login"
+      pass <- strOption $ long "pass" <> short 'p' <> help "User password"
+      pure $ (email, pass)
+    token = strOption $ long "token" <> short 't' <> help "API access token"
 
 optparser :: IO Options
 optparser = execParser opts
@@ -72,8 +69,10 @@ optparser = execParser opts
 main :: IO ()
 main = do
   Options {..} <- optparser
-  let myOptions = defaultOptions {o_max_request_rate_per_sec = 1.5} { o_verbosity = verb,
-    l_username = fromMaybe "" email, l_password = fromMaybe "" pass}
+  let myOptions' = defaultOptions {o_max_request_rate_per_sec = 1.5} { o_verbosity = verb }
+  let myOptions = case creds of
+                    Left (email, pass) -> myOptions' {l_username = email, l_password = pass}
+                    Right token -> myOptions' { l_access_token = token }
   x <- AP.displayConsoleRegions $ runVK myOptions $ fetch initPB AP.tick
   case x of
     (Left e) -> putStrLn $ show e
